9748d8c204f3b2313a6021db7822317707511f72
bariluz93 <bar.iluz@mail.huji.ac.il>
2022-06-09 15:59:20 +0300

    bolukbasy working

--- src/evaluate_translation.py
+++ src/evaluate_translation.py
@@ -1,22 +1,22 @@
 from sacrebleu.metrics import BLEU
 import sys
 sys.path.append("../../") # Adds higher directory to python modules path.
-from debias_files.consts import get_evaluate_translation_files, LANGUAGE_STR_MAP, parse_config, Language
+from debias_files.consts import get_evaluate_translation_files, LANGUAGE_STR_MAP, parse_config, Language, get_basic_configurations, TranslationModelsEnum
 from debias_files.detokenize import detokenize_matrix
 import argparse
 def evaluate_translation(language):
     # refs = []
     with open(TRANSLATED_DEBIASED, 'r') as d, open(TRANSLATED_NON_DEBIASED, 'r') as nd, open(
-            BLEU_GOLD_DATA, 'r') as g:
-        debiased = d.readlines()
-        sys_debiased = detokenize_matrix(debiased, LANGUAGE_STR_MAP[Language(int(language))])
-        # sys_debiased = d.readlines()
-        non_debiased = nd.readlines()
-        sys_non_debiased = detokenize_matrix(non_debiased, LANGUAGE_STR_MAP[Language(int(language))])
-        # sys_non_debiased = nd.readlines()
-        gold = g.readlines()
-        refs = detokenize_matrix(gold, LANGUAGE_STR_MAP[Language(int(language))])
-        # refs = g.readlines()
+            BLEU_GOLD_DATA, 'r') as gt, open(BLEU_GOLD_DATA_NON_TOKENIZED,'r') as gnt:
+        sys_debiased = d.readlines()
+        sys_non_debiased = nd.readlines()
+        refs = gnt.readlines()
+
+        if int(TRANSLATION_MODEL) == TranslationModelsEnum.NEMATUS.value:
+            sys_debiased = detokenize_matrix(sys_debiased, LANGUAGE_STR_MAP[Language(int(language))])
+            sys_non_debiased = detokenize_matrix(sys_non_debiased, LANGUAGE_STR_MAP[Language(int(language))])
+            refs = gt.readlines()
+            refs = detokenize_matrix(refs, LANGUAGE_STR_MAP[Language(int(language))])
     bleu = BLEU()
     print("debiased")
     print(bleu.corpus_score(sys_debiased, [refs]))
@@ -34,6 +34,6 @@ if __name__ == '__main__':
                  "collect_embedding_table= run translate to collect embedding table or not\n"
                  "print_line_nums= whether to print line numbers to output file in translate")
     args = parser.parse_args()
-    BLEU_SOURCE_DATA, BLEU_GOLD_DATA, TRANSLATED_DEBIASED, TRANSLATED_NON_DEBIASED = get_evaluate_translation_files(args.config_str)
-
+    BLEU_GOLD_DATA,BLEU_GOLD_DATA_NON_TOKENIZED, TRANSLATED_DEBIASED, TRANSLATED_NON_DEBIASED = get_evaluate_translation_files(args.config_str)
+    _, _, _, _, TRANSLATION_MODEL = get_basic_configurations(args.config_str)
     evaluate_translation( parse_config(args.config_str)["LANGUAGE"])
