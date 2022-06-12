c238919e75ad48f17e95ca3df56e518898b8b068
bariluz93 <bar.iluz@mail.huji.ac.il>
2022-06-12 18:50:46 +0300

    changed directory naed in mt-gender

--- scripts/evaluate_language.sh
+++ scripts/evaluate_language.sh
@@ -13,10 +13,9 @@ dataset=$1
 lang=$2
 trans_sys=$3
 debias_method=$4
-model=$5
 prefix=en-$lang
-prefix_debiased=${prefix}-debiased-${debias_method}_${model}
-prefix_non_debiased=${prefix}-non-debiased-${debias_method}_${model}
+prefix_debiased=${prefix}-debiased-${debias_method}_${trans_sys}
+prefix_non_debiased=${prefix}-non-debiased-${debias_method}_${trans_sys}
 
 
 # Prepare files for translation
@@ -44,8 +43,8 @@ $FAST_ALIGN_BASE/build/fast_align -i $trans_fn_non_debiased -d -o -v > $align_fn
 
 # Evaluate
 mkdir -p ../data/human/$trans_sys/$lang/
-out_fn_debiased=../data/human/$trans_sys/$lang/${lang}-debiased_${debias_method}_${model}.pred.csv
-out_fn_non_debiased=../data/human/$trans_sys/$lang/${lang}-non_debiased_${debias_method}_${model}.pred.csv
+out_fn_debiased=../data/human/$trans_sys/$lang/${lang}-debiased_${debias_method}_${trans_sys}.pred.csv
+out_fn_non_debiased=../data/human/$trans_sys/$lang/${lang}-non_debiased_${debias_method}_${trans_sys}.pred.csv
 echo "output path: ${out_fn_debiased}"
 echo "python load_alignments.py --ds=$dataset  --bi=$trans_fn_debiased --align=$align_fn_debiased --lang=$lang --out=$out_fn_debiased"
 echo "python load_alignments.py --ds=$dataset  --bi=$trans_fn_non_debiased --align=$align_fn_non_debiased --lang=$lang --out=$out_fn_non_debiased"
